7c88f55c129175bf01bd6a1bcd870b159dcb40ce
TrinhNam <ttnam957@gmail.com>
2024-01-04 10:24:28 +0700

    Can changing music, sfx volume in Settings

--- Assets/Scripts/MainMenu/MenuController.cs
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -12,18 +12,31 @@ namespace MainMenu
         [SerializeField] private AudioClip _mainMenuBackground;
         [SerializeField] private GameObject _settingsScreen;
         
+        private string p_musicVolume = "Music Volume";
+        private string p_sFXVolume = "SFX Volume";
+        private string p_masterVolume = "Master Volume";
 
         private void Start()
         {
             _highScoreText.GetComponent<TextMeshProUGUI>().SetText($"High Score = {PlayerPrefs.GetInt("HighScore")}");
-
-            _settingsScreen.SetActive(false);
+            
+            Debug.Log("music: " + PlayerPrefs.GetFloat(p_musicVolume));
+            Debug.Log("master: " + PlayerPrefs.GetFloat(p_masterVolume));
+            Debug.Log("sfx: " + PlayerPrefs.GetFloat(p_sFXVolume));
         }
 
         public void OpenSettingsSreen()
         {
             _settingsScreen.SetActive(true);
             this.gameObject.SetActive(false);
+        }
+
+        public void BackToMainMenu()
+        {
+            _settingsScreen.SetActive(false);
+            
+            this.gameObject.SetActive(true);
+            
         }
     }
 }
--- Assets/Scripts/MainMenu/SettingsManager.cs
+++ Assets/Scripts/MainMenu/SettingsManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Player;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
@@ -16,6 +18,23 @@ public class SettingsManager : MonoBehaviour
     private string p_sFXVolume = "SFX Volume";
     private string p_masterVolume = "Master Volume";
 
+    private void Start()
+    {
+        Debug.Log("music: " + PlayerPrefs.GetFloat(p_musicVolume));
+        Debug.Log("master: " + PlayerPrefs.GetFloat(p_masterVolume));
+        Debug.Log("sfx: " + PlayerPrefs.GetFloat(p_sFXVolume));
+        
+        _musicSlider.minValue = -40;
+        _musicSlider.maxValue = 0;
+        
+        _SFXSlider.minValue = -40;
+        _SFXSlider.maxValue = 0;
+        
+        _masterSlider.minValue = -40;
+        _masterSlider.maxValue = 0;
+        
+        LoadVolume();
+    }
 
     public void SetMusicVolume()
     {
@@ -24,35 +43,46 @@ public class SettingsManager : MonoBehaviour
         //AudioMixer's volume changes logarithmically, but the value of the slider changes linearly 
         //why 20?
         //Go back to Unity, and set value of the slider: min = 0.0001, max = 1
-        _musicSlider.minValue = -40;
-        _musicSlider.maxValue = 0;
+        
         // _audioMixer.SetFloat(p_musicVolume, Mathf.Log(volume) * 20);
         _audioMixer.SetFloat(p_musicVolume, volume);
+        PlayerPrefs.SetFloat(p_musicVolume, volume);
+        
+        Debug.Log("mody music: " + PlayerPrefs.GetFloat(p_musicVolume));
     }
 
     public void SetMasterVolume()
     {
         float volume = _masterSlider.value;
         
-        //AudioMixer's volume changes logarithmically, but the value of the slider changes linearly 
-        //why 20?
-        //Go back to Unity, and set value of the slider: min = 0.0001, max = 1
-        _musicSlider.minValue = -40;
-        _musicSlider.maxValue = 0;
-        // _audioMixer.SetFloat(p_musicVolume, Mathf.Log(volume) * 20);
+        
         _audioMixer.SetFloat(p_masterVolume, volume);
+        PlayerPrefs.SetFloat(p_masterVolume, volume);
+        
+        Debug.Log("mody master: " + PlayerPrefs.GetFloat(p_masterVolume));
     }
 
     public void SetSFXVolume()
     {
         float volume = _SFXSlider.value;
+        Debug.Log("mody sfx: " + volume);
         
-        //AudioMixer's volume changes logarithmically, but the value of the slider changes linearly 
-        //why 20?
-        //Go back to Unity, and set value of the slider: min = 0.0001, max = 1
-        _musicSlider.minValue = -40;
-        _musicSlider.maxValue = 0;
-        // _audioMixer.SetFloat(p_musicVolume, Mathf.Log(volume) * 20);
         _audioMixer.SetFloat(p_sFXVolume, volume);
+        PlayerPrefs.SetFloat(p_sFXVolume, volume);
+        
+        
+    }
+
+    private void LoadVolume()
+    {
+        //
+        _musicSlider.value = PlayerPrefs.GetFloat(p_musicVolume, 0);
+        Debug.Log("music volume: " + PlayerPrefs.GetFloat(p_musicVolume));
+        Debug.Log("music slider: " + _musicSlider.value);
+        _SFXSlider.value = PlayerPrefs.GetFloat(p_sFXVolume, 0);
+        _masterSlider.value = PlayerPrefs.GetFloat(p_masterVolume, 0);
+        
+        
+        
     }
 }
